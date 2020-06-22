f2f38fcf7c15cc098d00f20f00b0b9845923e425
Martin Wernstål <m4rw3r@gmail.com>
2020-06-22 18:07:24 +0200

    fix: type/lint fixes

--- src/index.js
+++ src/index.js
@@ -1,36 +1,57 @@
 /* @flow */
 
+import type { DefinitionNode, FragmentSpreadNode } from "graphql/language";
+
 import { createFilter } from "@rollup/pluginutils";
 import { Kind, Source, parse, visit } from "graphql/language";
 
-const COMMENT_PATTERN = /#([^\n\r]*)/g;
-const IMPORT_PATTERN = /import\s*{((?:\s*[_A-Za-z][_0-9A-Za-z]*\s*,?\s*)+)}\s*from\s*(['"])([^\n\r]*)\2/;
+type TransformedSource = {
+  code: string,
+  map: {
+    mappings: string,
+  },
+};
 
-function graphql(options = {}) {
-  let filter = createFilter(options.include || "**/*.graphql", options.exclude);
+type RollupPlugin = {
+  name: string,
+  transform: (source: string, id: string) => ?TransformedSource,
+};
+
+type Options = {
+  include?: string | RegExp | Array<string | RegExp>,
+  exclude?: string | RegExp | Array<string | RegExp>,
+};
+
+const COMMENT_PATTERN = /#([^\n\r]*)/g;
+const IMPORT_PATTERN = /import\s*{((?:\s*[_A-Za-z]\w*\s*,?\s*)+)}\s*from\s*(['"])([^\n\r]*)\2/;
+
+export function graphql(options: Options = {}): RollupPlugin {
+  const filter = createFilter(options.include || "**/*.graphql", options.exclude);
 
   return {
     name: "graphql-ast-import",
-    transform(gqlSource, id) {
+    transform(gqlSource: string, id: string): ?TransformedSource {
       if (!filter(id)) {
         return;
       }
 
-      const source = new Source(gqlSource, id)
+      const source = new Source(gqlSource, id);
       const ast = parse(source, { noLocation: true });
       const comments = gqlSource.match(COMMENT_PATTERN) || [];
-      const imports = comments.map(line => {
+      const imports = comments.map((line: string): ?string => {
         // TODO: Error handling with this
         const match = line.match(IMPORT_PATTERN);
 
-        if(match) {
-          const names = match[1].split(/[, ]/g).map(s => s.trim()).filter(x => x);
-
-          return `import { ${names.join(", ")} } from ${JSON.stringify(match[3])};`;
+        if (!match) {
+          return null;
         }
-      }).filter(x => x);
 
-      const definitions = ast.definitions.map(d => {
+        const names = match[1].split(/[, ]/g).map((s: string): string => s.trim()).filter(Boolean);
+
+        return `import { ${names.join(", ")} } from ${JSON.stringify(match[3])};`;
+      }).filter(Boolean);
+
+      const definitions = ast.definitions.map((d: DefinitionNode): string => {
         if (!d.name) {
           throw new Error("All queries/mutations must be named");
         }
@@ -39,7 +60,7 @@ function graphql(options = {}) {
         const used = [];
 
         visit(d, {
-          FragmentSpread(spread) {
+          FragmentSpread(spread: FragmentSpreadNode): void {
             const { name: { value } } = spread;
 
             if (!used.includes(value)) {
@@ -51,7 +72,7 @@ function graphql(options = {}) {
         // All imports are documents, so merge the definitions
         return `export const ${name} = {
   kind: ${JSON.stringify(Kind.DOCUMENT)},
-  definitions: [].concat(${used.map(name => `${name}.definitions`).concat([JSON.stringify(d)]).join(", ")})
+  definitions: [].concat(${used.map((name: string): string => `${name}.definitions`).concat([JSON.stringify(d)]).join(", ")})
 };`;
       });
 
@@ -62,6 +83,6 @@ function graphql(options = {}) {
         // TODO: Source maps
         map: { mappings: "" },
       };
-    }
+    },
   };
 }
