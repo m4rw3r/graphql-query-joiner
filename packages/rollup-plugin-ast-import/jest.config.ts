571044a046b54b4bad81f142801a8ebb813765c4
Martin Wernstål <m4rw3r@gmail.com>
2024-02-10 13:46:47 +0100

    chore: converted imported package to use the shared monorepo config

--- packages/rollup-plugin-ast-import/jest.config.ts
+++ packages/rollup-plugin-ast-import/jest.config.ts
@@ -1,21 +1,3 @@
-import type { JestConfigWithTsJest } from "ts-jest";
+import defaultConfig from "../../jest.config";
 
-/*
- * For a detailed explanation regarding each configuration property and type check, visit:
- * https://jestjs.io/docs/configuration
- */
-const jestConfig: JestConfigWithTsJest = {
-  clearMocks: true,
-  collectCoverage: true,
-  coverageDirectory: "coverage",
-  coverageProvider: "v8",
-  testEnvironment: "node",
-  transform: {
-    "^.+\\.(t|j)sx?$": [
-      "ts-jest",
-      { isolatedModules: true, diagnostics: { ignoreCodes: ["TS151001"] } },
-    ],
-  },
-};
-
-export default jestConfig;
+export default defaultConfig;
